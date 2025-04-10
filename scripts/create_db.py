2e1ecbe28e3ae87760fd1629c17ac643f67f8d77
Modestas Filipavičius <mfilipav@ethz.ch>
2025-04-10 10:48:39 +0000

    enabled default NLG paraphraser

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -11,17 +11,20 @@ local_file = "travel2.sqlite"
 backup_file = "travel2.backup.sqlite"
 overwrite = True
 if overwrite or not os.path.exists(local_file):
+    print("create db at: ", db_url)
     response = requests.get(db_url)
     response.raise_for_status()  # Ensure the request was successful
+    print("write db to local file: ", local_file)
     with open(local_file, "wb") as f:
         f.write(response.content)
     # Backup - we will use this to "reset" our DB in each section
+    print("copy db from local file to: ", backup_file)
     shutil.copy(local_file, backup_file)
 
 
 conn = sqlite3.connect(local_file)
 cursor = conn.cursor()
-
+print("read sql tables into PD dataframe")
 tables = pd.read_sql(
     "SELECT name FROM sqlite_master WHERE type='table';", conn
 ).name.tolist()
@@ -48,6 +51,7 @@ for table_name, df in tdf.items():
     df.to_sql(table_name, conn, if_exists="replace", index=False)
 del df
 del tdf
+print("done")
 conn.commit()
 conn.close()
 
